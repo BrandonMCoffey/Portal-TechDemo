00fc3a36186fea65a2c04d32587e692af193a619
Brandon Coffey <bmc180001@utdallas.edu>
2021-03-11 19:05:18 -0600

    Minor adjustments to Portal Laser

--- Assets/Scripts/Portals/PortalLaser.cs
+++ Assets/Scripts/Portals/PortalLaser.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 namespace Assets.Scripts.Portals {
     [RequireComponent(typeof(LineRenderer))]
-    public class PortalLineLaser : MonoBehaviour {
+    public class PortalLaser : MonoBehaviour {
         [SerializeField] private Material _portal0 = null;
         [SerializeField] private Material _portal1 = null;
         [SerializeField] private AnimationCurve _durationCurve = AnimationCurve.Constant(0, 1, 1);
         [SerializeField] private TransformReference _fromLocation = null;
-        [SerializeField] private PortalVariable _toLocation = null;
 
         private Coroutine _coroutine;
 
@@ -19,14 +18,13 @@ namespace Assets.Scripts.Portals {
         {
             _lineRenderer = GetComponent<LineRenderer>();
             if (_fromLocation == null) Debug.Log("[" + GetType().Name + "] From Location Variable missing on " + name);
-            if (_toLocation == null) Debug.Log("[" + GetType().Name + "] To Location Variable missing on " + name);
         }
 
-        public void DrawLine()
+        public void DrawLine(PortalVariable portalLocation)
         {
-            _lineRenderer.material = _toLocation.PortalID == 0 ? _portal0 : _portal1;
+            _lineRenderer.material = portalLocation.PortalID == 0 ? _portal0 : _portal1;
             _lineRenderer.SetPosition(0, _fromLocation.Position);
-            _lineRenderer.SetPosition(1, _toLocation.Position);
+            _lineRenderer.SetPosition(1, portalLocation.Position);
             if (_coroutine != null) {
                 StopCoroutine(_coroutine);
             }
